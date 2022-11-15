e33d6f187f86b73dca401a5e2a5a96f330a4503b
Carlos <carlosemaya@gmail.com>
2022-11-14 22:50:25 -0300

    commit inicial

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import styled from "style-components";
+
+export const Cabecalho = styled.H1`
+    color: blue;
+`;
+
+function Header() {
+    return (
+        <div>
+            <h1>Ola Mundo</h1>
+        </div>
+    )
+}
+
+export default Header
