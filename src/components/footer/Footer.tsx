bc1c05aecd0adf450bc18b9197d19c8ed69f86d5
Carlos <carlosemaya@gmail.com>
2022-11-22 21:32:13 -0300

    Bandeiras adicionadas aos botões e estilização dos mesmos

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,24 @@
-import { ContainerButton } from "./Styled"
+import { ContainerButtons } from "./Styled"
+import BrazilFlag from "../../assets/images/brazil-flag.png"
+import SpainFlag from "../../assets/images/spain-flag.png"
+import UsaFlag from "../../assets/images/usa-flag.png"
 
 function Footer() {
     return (
-        <ContainerButton>
-            <div>Português</div>
-            <div>Espanhol</div>
-            <div>Inglês</div>
-        </ContainerButton>
+        <ContainerButtons>
+            <div>
+                <img src={BrazilFlag} />
+                <p>Português</p>
+            </div>
+            <div>
+                <img src={SpainFlag} />
+                <p>Espanhol</p>
+            </div>
+            <div>
+                <img src={UsaFlag} />
+                <p>Inglês</p>
+            </div>
+        </ContainerButtons>
     )
 }
 
--- src/components/footer/Styled.ts
+++ src/components/footer/Styled.ts
@@ -1,12 +1,15 @@
 import styled from "styled-components"
 
-export const ContainerButton = styled.div`
+export const ContainerButtons = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
     margin: 5em 0 0 0;
 
     div {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
         margin-right: 2em;
         padding: 0.5em 1em;
         background-color: rgb(39,39,42);
@@ -14,5 +17,9 @@ export const ContainerButton = styled.div`
         color: white;
         font-family: Inter, sans-serif;
         font-weight: 500;
+    }
+
+    img {
+        padding-right: 0.5em;
     }
 `;
