37951ca864ca080bb0988101f4be1dbcc8707d0e
Carlos <carlosemaya@gmail.com>
2022-11-26 20:57:26 -0300

    Inicio da estilização da lista de tecnologias

--- src/components/lista/Styled.ts
+++ src/components/lista/Styled.ts
@@ -1,5 +1,20 @@
 import styled from "styled-components"
 
 export const ContainerLista = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     
+
+    ul {
+        
+    }
+
+    li {
+        margin-right: 1em;
+        color: white;
+        font-family: Inter, sans-serif;
+        font-weight: 700;
+        font-size: 1.25em;
+    }
 `;
