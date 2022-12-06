35ae9960537dccfc087e04ad036fc651823929e7
Carlos <carlosemaya@gmail.com>
2022-12-05 21:32:29 -0300

    useState hook para mudar linguagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react"
 import Header from "./components/header/Header"
 import Content from "./components/content/Content"
 import GlobalStyle from "./components/globals/Styled"
+
+const languages = [{
+  pt: "Olá, meu nome é Carlos Eduardo da Maia e eu sou um desenvolvedor Front-End. Tecnologias que tenho experiência:",
+  es: "Hola, mi nombre es Carlos Eduardo da Maia y soy desarrollador Front-End. Tecnologías con las que tengo experiencia:",
+  en: "Hello, my name is Carlos Eduardo da Maia and I am a Front-End developer. Technologies I have experience with:"
+}]
+
 
 function App() {
 
@@ -8,7 +16,11 @@ function App() {
     <div>
       <GlobalStyle />
       <Header />
+      <div>
+        <h1>{languages.indexOf(2)}</h1>
+      </div>
       <Content />
+      
     </div>
   )
 }
