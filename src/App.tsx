1f0280c6fc8da38209df3438dc91d17f6e0d6707
Carlos <carlosemaya@gmail.com>
2023-01-10 13:59:30 -0300

    Teste rápido com arrays

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Header from "./components/header/Header"
 import Content from "./components/content/Content"
 import GlobalStyle from "./components/globals/Styled"
 
-const languages = [{
-  pt: "Olá, meu nome é Carlos Eduardo da Maia e eu sou um desenvolvedor Front-End. Tecnologias que tenho experiência:",
-  es: "Hola, mi nombre es Carlos Eduardo da Maia y soy desarrollador Front-End. Tecnologías con las que tengo experiencia:",
-  en: "Hello, my name is Carlos Eduardo da Maia and I am a Front-End developer. Technologies I have experience with:"
-}]
+const languages = [
+  "Olá, meu nome é Carlos Eduardo da Maia e eu sou um desenvolvedor Front-End. Tecnologias que tenho experiência:",
+  "Hola, mi nombre es Carlos Eduardo da Maia y soy desarrollador Front-End. Tecnologías con las que tengo experiencia:",
+  "Hello, my name is Carlos Eduardo da Maia and I am a Front-End developer. Technologies I have experience with:",
+]
 
+console.log(languages[0])
 
 function App() {
 
@@ -17,7 +18,6 @@ function App() {
       <GlobalStyle />
       <Header />
       <Content />
-      
     </div>
   )
 }
